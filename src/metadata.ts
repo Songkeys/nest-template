643b5b5096e95804a8f2a637827a108e764a6a39
Songkeys <songv587@gmail.com>
2023-07-21 13:06:20 +0800

    chore: swc

---
+++ src/metadata.ts
@@ -0,0 +1,5 @@
+/* eslint-disable */
+export default async () => {
+    const t = {};
+    return { "@nestjs/swagger": { "models": [], "controllers": [[import("./module/http/ping/ping.controller"), { "PingController": { "ping": { type: String } } }], [import("./module/prisma/prisma.controller"), { "PrismaController": { "getMetrics": { type: String } } }]] } };
+};
